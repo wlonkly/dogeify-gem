1ee68f8037ce3b2c18048372b10b9f93ea29b7e3
Rich Lafferty <rlafferty@pagerduty.com>
2017-06-07 10:29:31 -0400

    Initial commit

---
+++ lib/dogeify.rb
@@ -0,0 +1,36 @@
+require 'dogeify/version'
+require 'engtagger'
+
+class Dogeify
+  ADJECTIVES = %w(so such very much many).freeze
+
+  def initialize
+    @tagger = EngTagger.new
+  end
+
+  def process(str)
+    # Convert input to lowercase.
+    str = str.downcase
+
+    # Extract nouns, prefixing each with one of the
+    # above adjectives into sentences of 2 words.
+    tagged_str = @tagger.add_tags(str)
+    phrases = @tagger.get_nouns(tagged_str).keys
+    phrases = phrases.each_with_index.map do |phrase, i|
+      "#{adjective(i)} #{phrase}."
+    end
+
+    # End every input with "wow".
+    phrases << 'wow.'
+
+    # Return a string, separating each sentence
+    # with a space.
+    phrases.join(' ')
+  end
+
+  private
+
+  def adjective(i)
+    ADJECTIVES[i % ADJECTIVES.size]
+  end
+end
